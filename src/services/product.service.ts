b63b311f7f9b377a3270a7673543af9749e7067b
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-23 01:35:01 +0300

    feat(product.service): Implement product creation service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,17 @@
+import { ServiceError } from "../errors/service.error";
+import { Product } from "../models/product.model";
 import { ProductTypes } from "../types/product.types";
 
+// TODO: This is not finished yet, test it.
 const createProductService = async (product: ProductTypes.ProductInput) => {
+  if (!product) {
+    throw new ServiceError(400, "Product details are needed.");
+  }
 
+  const newProduct = new Product(product);
+  await newProduct.save();
+
+  return newProduct;
 };
+
+export { createProductService };
