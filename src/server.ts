5d83a0b87b9ea75745d12317951ab8f6d26c566e
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-23 01:40:19 +0300

    feat(server): Add product routes and invalid JSON handler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import ip from "ip";
 import connectDatabase from "./db";
+import { productRoutes } from "./routes/product.routes";
+import handleInvalidJson from "./middlewares/invalidsyntax.middleware";
 
 const app: Express = express();
 const port = process.env.API_PORT || 3000;
@@ -8,10 +10,14 @@ const port = process.env.API_PORT || 3000;
 connectDatabase(__filename);
 
 app.use(express.json());
-
+app.use("/products", productRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
+// TODO: Invalid JSON syntax errors should be handled via a middleware
+// because it gives the error way before the endpoint error handler handles it.
+// Though it doesn't work in this state, fix it whenever you can.
+app.use(handleInvalidJson);
 
 app.listen(port, () => {
   console.log(`Server is running on http://${ip.address()}:${port}`);
