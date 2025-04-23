bad8ecb9aa4ed1f88105f8725854d860c35eabdc
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-23 05:29:01 +0300

    feat(product.controller): Use getProductService for getting products, optionally with the ID

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { createProductService } from "../services/product.service";
+import {
+  createProductService,
+  getProductService,
+} from "../services/product.service";
 import { handleControllerError } from "../utils/error-response";
 
 const postProductController = async (
@@ -15,4 +18,38 @@ const postProductController = async (
   }
 };
 
-export { postProductController };
+const getAllProductsController = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const products = await getProductService();
+    res
+      .status(201)
+      .json({ message: "All products are listed.", data: { products } });
+  } catch (error) {
+    handleControllerError(res, error, true);
+    return;
+  }
+};
+
+const getProductByIdController = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const product = await getProductService({ _id: req.params.id });
+    res
+      .status(201)
+      .json({ message: "Product is displayed.", data: { product } });
+  } catch (error) {
+    handleControllerError(res, error, true);
+    return;
+  }
+};
+
+export {
+  postProductController,
+  getAllProductsController,
+  getProductByIdController,
+};
