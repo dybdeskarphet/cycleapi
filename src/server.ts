08832e196d300ab932673843205f1b85e545085c
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-23 04:18:48 +0300

    fix: Fix JSON validation

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,37 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import ip from "ip";
 import connectDatabase from "./db";
 import { productRoutes } from "./routes/product.routes";
-import handleInvalidJson from "./middlewares/invalidsyntax.middleware";
+import { ServiceError } from "./errors/service.error";
 
 const app: Express = express();
 const port = process.env.API_PORT || 3000;
 
 connectDatabase(__filename);
 
-app.use(express.json());
+// Check if the request is a valid JSON
+app.use(
+  express.json({
+    verify: (
+      req: Request,
+      res: Response,
+      buf: Buffer,
+      encoding: BufferEncoding,
+    ) => {
+      try {
+        JSON.parse(buf.toString(encoding));
+        return;
+      } catch (e) {
+        res.status(400).json({ message: "Invalid JSON format." });
+        return;
+      }
+    },
+  }),
+);
+
 app.use("/products", productRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
-// TODO: Invalid JSON syntax errors should be handled via a middleware
-// because it gives the error way before the endpoint error handler handles it.
-// Though it doesn't work in this state, fix it whenever you can.
-app.use(handleInvalidJson);
 
 app.listen(port, () => {
   console.log(`Server is running on http://${ip.address()}:${port}`);
