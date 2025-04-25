4251877384e59a43ac0d697b4f98925d942ef5a8
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-25 23:55:42 +0300

    feat(product.service): Add sale creation service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,7 +1,8 @@
 import { ServiceError } from "../errors/service.error";
 import { Product } from "../models/product.model";
+import { Unit } from "../models/unit.model";
 import { ProductTypes } from "../types/product.types";
-import { Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 // TODO: This is not finished yet, test it.
 const createProductService = async (product: ProductTypes.ProductInput) => {
@@ -16,12 +17,28 @@ const createProductService = async (product: ProductTypes.ProductInput) => {
 };
 
 const getProductService = async (filters: Record<string, any> = {}) => {
-  const result = Product.find(filters).exec();
-  if (result) {
-    return result;
-  } else {
+  const result = await Product.find(filters).exec();
+  if (!result) {
     throw new ServiceError(404, "Couldn't find any product(s).");
   }
+
+  return result;
 };
 
-export { createProductService, getProductService };
+const postNewSaleService = async (
+  productId: string,
+  unitProps: Record<string, any>,
+) => {
+  const product = await Product.findById(productId).exec();
+  if (!product) {
+    throw new ServiceError(404, "Couldn't find any product(s).");
+  }
+
+  // TODO: Use the productId in `new Unit()` instead of needing in it in a body
+  const unit = new Unit({ product: productId, ...unitProps });
+  await unit.save();
+  product.sales.push(unit._id as mongoose.Types.ObjectId);
+  await product.save();
+};
+
+export { createProductService, getProductService, postNewSaleService };
