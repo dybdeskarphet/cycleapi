31c9104d2fb554858a36ca8a74675ad849b8f1d0
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-04-26 19:04:04 +0300

    feat(product.routes): Add DELETE product by ID endpoint

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  deleteProductByIdController,
   deleteSaleByIdController,
   getAllProductsController,
   getProductByIdController,
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.post("/", postProductController);
 router.get("/:id", getProductByIdController);
+router.delete("/:id", deleteProductByIdController);
 router.get("/", getAllProductsController);
 router.post("/:id/sales", postNewSaleController);
 router.delete("/:productId/sales/:saleId", deleteSaleByIdController);
