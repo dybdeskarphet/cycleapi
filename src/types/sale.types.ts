f0f7d39f514fc2dd008abce32a7bfa6f57ffdd73
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-03 04:55:01 +0300

    feat(sale.types): Add new schemas with zod for restoring sales

--- src/types/sale.types.ts
+++ src/types/sale.types.ts
@@ -1,5 +1,6 @@
 import { Types, HydratedDocument } from "mongoose";
 import { ProductDocument } from "./product.types";
+import { z } from "zod";
 
 export interface ISale {
   product: Types.ObjectId | ProductDocument;
@@ -13,5 +14,23 @@ export interface IMiniSale {
   amount: number;
   createdAt: string;
 }
+
+export const RestoreSaleInputSchema = z
+  .object({
+    _id: z.instanceof(Types.ObjectId).optional(),
+    amount: z.number(),
+    customPrice: z.number().optional(),
+    createdAt: z.string().datetime(),
+    updatedAt: z.string().datetime(),
+  })
+  .passthrough();
+
+export const RestoreSaleInputArraySchema = z.array(RestoreSaleInputSchema);
+
+export type RestoreSaleInputSchemaType = z.infer<typeof RestoreSaleInputSchema>;
+
+export type RestoreSaleInputArraySchemaType = z.infer<
+  typeof RestoreSaleInputArraySchema
+>;
 
 export type SaleDocument = HydratedDocument<ISale>;
