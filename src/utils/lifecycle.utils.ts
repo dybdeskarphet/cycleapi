05ca74dc4049839efab776fb8d0e8ccd34b6c65f
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-05 00:48:07 +0300

    refactor(lifecycle.utils): Minimze moving LR slope calculator util

--- src/utils/lifecycle.utils.ts
+++ src/utils/lifecycle.utils.ts
@@ -27,39 +27,41 @@ export const getLinearRegressionSlopeOfSales = async (
   sales: IMiniSale[],
   interval: string,
 ) => {
-  const mean_y = await calculateAverageSales(sales, sales.length);
-  const mean_x = sales.reduce((acc, val) => {
+  let mean_x = 0;
+  let mean_y = 0;
+  let sxx = 0;
+  let sxy = 0;
+
+  for (let i = 0; i < sales.length; i++) {
     const currentDate = differenceInDays(
-      parseISO(convertIntervalDateToISODate(val.createdAt, interval) as string),
+      parseISO(
+        convertIntervalDateToISODate(sales[i].createdAt, interval) as string,
+      ),
       parseISO(
         convertIntervalDateToISODate(sales[0].createdAt, interval) as string,
       ),
     );
-    return acc + currentDate;
-  }, 0);
 
-  const sxx = sales.reduce((acc, val) => {
+    mean_y += sales[i].amount;
+    mean_x += currentDate;
+  }
+
+  mean_x = mean_x / sales.length;
+  mean_y = mean_y / sales.length;
+
+  for (let i = 0; i < sales.length; i++) {
     const currentDate = differenceInDays(
-      parseISO(convertIntervalDateToISODate(val.createdAt, interval) as string),
+      parseISO(
+        convertIntervalDateToISODate(sales[i].createdAt, interval) as string,
+      ),
       parseISO(
         convertIntervalDateToISODate(sales[0].createdAt, interval) as string,
       ),
     );
 
-    console.log(currentDate);
-
-    return acc + Math.pow(currentDate - mean_x, 2);
-  }, 0);
-
-  const sxy = sales.reduce((acc, val) => {
-    const currentDate = differenceInDays(
-      parseISO(convertIntervalDateToISODate(val.createdAt, interval) as string),
-      parseISO(
-        convertIntervalDateToISODate(sales[0].createdAt, interval) as string,
-      ),
-    );
-    return (currentDate - mean_x) * (val.amount - mean_y) + acc;
-  }, 0);
+    sxx += Math.pow(currentDate - mean_x, 2);
+    sxy += (currentDate - mean_x) * (sales[i].amount - mean_y);
+  }
 
   return sxy / sxx;
 };
