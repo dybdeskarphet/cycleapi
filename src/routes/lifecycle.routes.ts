2f5ae4ab47a96f2c7456425f45fb9452bf9f041a
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-04 05:39:13 +0300

    feat(lifecycle.routes): Use LR slopes controller in its endpoint

--- src/routes/lifecycle.routes.ts
+++ src/routes/lifecycle.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getGrowthRateController,
   getMovingAveragesController,
+  getMovingLinearRegressionSlopesController,
   getSalesAccelerationController,
 } from "../controllers/lifecycle.controller";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/:id/moving-averages", getMovingAveragesController);
 router.post("/:id/growth-rates", getGrowthRateController);
 router.post("/:id/acceleration-rates", getSalesAccelerationController);
+router.post("/:id/lr-slopes", getMovingLinearRegressionSlopesController);
 
 export { router as lifecycleRoutes };
