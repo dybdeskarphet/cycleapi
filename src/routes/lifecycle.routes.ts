1fca0d3458dae24b060bc43a78045bb10f261dc7
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-05 04:11:16 +0300

    feat(lifecycle.routes): Add the endpoint for phase detection

--- src/routes/lifecycle.routes.ts
+++ src/routes/lifecycle.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getGroupedLinearRegressionSlopesController,
   getGrowthRateController,
   getMovingAveragesController,
   getMovingLinearRegressionSlopesController,
@@ -12,5 +13,6 @@ router.post("/:id/moving-averages", getMovingAveragesController);
 router.post("/:id/growth-rates", getGrowthRateController);
 router.post("/:id/acceleration-rates", getSalesAccelerationController);
 router.post("/:id/lr-slopes", getMovingLinearRegressionSlopesController);
+router.post("/:id/phases-with-lr", getGroupedLinearRegressionSlopesController);
 
 export { router as lifecycleRoutes };
