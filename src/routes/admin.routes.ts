6efa7e5de7115fe971378f0882b5fe06dfba14a6
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-16 21:41:53 +0300

    feat(admin.routes): Use token generation route

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { requireAdminAuth } from "../middlewares/admin.middleware";
+import { generateTokenController } from "../controllers/admin.controller";
+
+const router = express.Router();
+
+router.post("/generate-token", requireAdminAuth, generateTokenController);
+
+export { router as adminRoutes };
