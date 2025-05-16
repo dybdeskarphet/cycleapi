dd91c53eb5652423a3af8ce801f30c162ac0d57f
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-16 21:39:46 +0300

    feat(admin.service): Add token generation service

---
+++ src/services/admin.service.ts
@@ -0,0 +1,27 @@
+import { randomBytes } from "node:crypto";
+import { IToken, TokenRequestBody } from "../types/token.types";
+import { Token } from "../models/token.model";
+import ms, { StringValue } from "ms";
+import { ApiError } from "../errors/api.error";
+import { StatusCodes } from "http-status-codes";
+import { ErrorEntries } from "../constants/messages.constants";
+
+export const generateTokenService = async (tokenBody: TokenRequestBody.TS) => {
+  const token = randomBytes(32).toString("hex");
+  // NOTE: vercel/ms doesn't have a validation system, but it
+  // returns undefined if the string is not StringValue
+  // so this is the only way to validate it.
+  const timeoutMs = ms(tokenBody.timeoutMs as StringValue);
+  if (!timeoutMs || timeoutMs <= 0) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, ErrorEntries.INVALID_TIMEOUT);
+  }
+
+  const newToken = new Token({
+    token,
+    scopes: tokenBody.scopes,
+    timeoutMs: timeoutMs,
+  });
+
+  await newToken.save();
+  return newToken;
+};
