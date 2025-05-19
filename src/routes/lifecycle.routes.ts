2ceecfb7b02cca61e2a84c896fecdf663b97de0b
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-19 05:39:29 +0300

    feat: Use requireScope with routes

--- src/routes/lifecycle.routes.ts
+++ src/routes/lifecycle.routes.ts
@@ -6,13 +6,35 @@ import {
   getMovingLinearRegressionSlopesController,
   getSalesAccelerationController,
 } from "../controllers/lifecycle.controller";
+import { requireScope } from "../middlewares/admin.middleware";
+import { Scopes } from "../enums/scopes.enum";
 
 const router = express.Router();
 
-router.post("/:id/moving-averages", getMovingAveragesController);
-router.post("/:id/growth-rates", getGrowthRateController);
-router.post("/:id/acceleration-rates", getSalesAccelerationController);
-router.post("/:id/lr-slopes", getMovingLinearRegressionSlopesController);
-router.post("/:id/phases-with-lr", getGroupedLinearRegressionSlopesController);
+router.post(
+  "/:id/moving-averages",
+  requireScope([Scopes.read_products, Scopes.read_sales]),
+  getMovingAveragesController,
+);
+router.post(
+  "/:id/growth-rates",
+  requireScope([Scopes.read_products, Scopes.read_sales]),
+  getGrowthRateController,
+);
+router.post(
+  "/:id/acceleration-rates",
+  requireScope([Scopes.read_products, Scopes.read_sales]),
+  getSalesAccelerationController,
+);
+router.post(
+  "/:id/lr-slopes",
+  requireScope([Scopes.read_products, Scopes.read_sales]),
+  getMovingLinearRegressionSlopesController,
+);
+router.post(
+  "/:id/phases-with-lr",
+  requireScope([Scopes.read_products, Scopes.read_sales]),
+  getGroupedLinearRegressionSlopesController,
+);
 
 export { router as lifecycleRoutes };
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -11,17 +11,47 @@ import {
   postNewSaleController,
   restoreOldSalesController,
 } from "../controllers/sales.controller";
+import { requireScope } from "../middlewares/admin.middleware";
+import { Scopes } from "../enums/scopes.enum";
 
 const router = express.Router();
 
-router.post("/", postProductController);
-router.get("/:id", getProductByIdController);
-router.delete("/:id", deleteProductByIdController);
-router.get("/", getProductsController());
-router.post("/filter", getProductsController(true));
-router.get("/:id/sales/:interval", getSalesByProductIdController);
-router.post("/:id/sales", postNewSaleController);
-router.delete("/:productId/sales/:saleId", deleteSaleByIdController);
-router.put("/:productId/sales/restore", restoreOldSalesController);
+router.post("/", requireScope([Scopes.write_products]), postProductController);
+router.get(
+  "/:id",
+  requireScope([Scopes.read_products]),
+  getProductByIdController,
+);
+router.delete(
+  "/:id",
+  requireScope([Scopes.read_products, Scopes.delete_products]),
+  deleteProductByIdController,
+);
+router.get("/", requireScope([Scopes.read_products]), getProductsController());
+router.post(
+  "/filter",
+  requireScope([Scopes.read_products]),
+  getProductsController(true),
+);
+router.get(
+  "/:id/sales/:interval",
+  requireScope([Scopes.read_sales]),
+  getSalesByProductIdController,
+);
+router.post(
+  "/:id/sales",
+  requireScope([Scopes.write_sales]),
+  postNewSaleController,
+);
+router.delete(
+  "/:productId/sales/:saleId",
+  requireScope([Scopes.read_sales, Scopes.delete_sales]),
+  deleteSaleByIdController,
+);
+router.put(
+  "/:productId/sales/restore",
+  requireScope([Scopes.read_sales, Scopes.write_sales]),
+  restoreOldSalesController,
+);
 
 export { router as productRoutes };
