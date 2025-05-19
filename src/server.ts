ab66a59fc30519438ec1ee73022494d193f40109
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-19 03:28:56 +0300

    refactor(server): Simplify JSON verification and use error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -6,31 +6,16 @@ import { lifecycleRoutes } from "./routes/lifecycle.routes";
 import { openApiDocument } from "./docs/openapi";
 import path from "path";
 import { adminRoutes } from "./routes/admin.routes";
+import { errorHandler } from "./middlewares/error-handler.middlware";
+import "body-parser";
+import { jsonVerify } from "./utils/express.utils";
 
 const app: Express = express();
 const port = process.env.API_PORT || 3000;
 
 await connectDatabase(__filename);
 
-// Check if the request is a valid JSON
-app.use(
-  express.json({
-    verify: (
-      req: Request,
-      res: Response,
-      buf: Buffer,
-      encoding: BufferEncoding,
-    ) => {
-      try {
-        JSON.parse(buf.toString(encoding));
-        return;
-      } catch (e) {
-        res.status(400).json({ message: "Invalid JSON format." });
-        return;
-      }
-    },
-  }),
-);
+app.use(express.json({ verify: jsonVerify }));
 
 app.get("/api-docs.json", (req, res) => {
   res.json(openApiDocument);
@@ -40,11 +25,6 @@ app.get("/api-docs", (req, res) => {
   res.sendFile(path.join(__dirname, "../docs/rapidoc.html"));
 });
 
-app.get("/", (req: Request, res: Response) => {
-  // NOTE: Change it to /api/v1 before release.
-  res.redirect("/api-docs");
-});
-
 app.get("/api/v1", (req: Request, res: Response) => {
   res.send("Go to /api-docs to see the full documentation.");
 });
@@ -52,7 +32,8 @@ app.get("/api/v1", (req: Request, res: Response) => {
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/lifecycle", lifecycleRoutes);
 app.use("/api/v1/admin", adminRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://${ip.address()}:${port}`);
+  console.log(`Server is running on http://${ip.address()}:${port}/api-docs`);
 });
