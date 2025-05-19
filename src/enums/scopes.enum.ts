ef783678ccaf21f65bb798781b5cf668d544960e
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-19 05:34:57 +0300

    feat(scopes.enum): Add new enums for future use

--- src/enums/scopes.enum.ts
+++ src/enums/scopes.enum.ts
@@ -2,4 +2,7 @@ export enum Scopes {
   read_products = "read:products",
   write_products = "write:products",
   delete_products = "delete:products",
+  read_sales = "read:sales",
+  write_sales = "write:sales",
+  delete_sales = "delete:sales",
 }
