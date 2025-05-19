c0c04b667acc9de5c35e642c3160b242868f7c5d
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-19 05:35:24 +0300

    feat(admin.middlware): Add requireScope middleware to be used with routes

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -3,6 +3,10 @@ import "dotenv/config";
 import { ApiError } from "../errors/api.error";
 import { StatusCodes } from "http-status-codes";
 import { ErrorEntries } from "../constants/messages.constants";
+import { Scopes } from "../enums/scopes.enum";
+import { z } from "zod";
+import { handleZodParsed } from "../utils/express.utils";
+import { getTokenByItselfService } from "../services/token.service";
 
 export const requireAdminAuth = (
   req: Request,
@@ -10,7 +14,9 @@ export const requireAdminAuth = (
   next: NextFunction,
 ) => {
   const adminToken = process.env.ADMIN_API_TOKEN;
-  const token = req.header("x-admin-api-key");
+  const token = handleZodParsed(
+    z.string().safeParse(req.header("x-admin-api-key")),
+  );
 
   if (token !== adminToken) {
     throw new ApiError(StatusCodes.UNAUTHORIZED, ErrorEntries.UNAUTHORIZED);
@@ -20,3 +26,42 @@ export const requireAdminAuth = (
 };
 
 // TODO: Now it's time to write the requireScope middleware
+
+export const requireScope = (requiredScopes: Scopes[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const tokenString = handleZodParsed(
+        z
+          .string({ message: "You have to provide an API token." })
+          .safeParse(req.header("x-api-key")),
+      );
+
+      const token = await getTokenByItselfService(tokenString);
+
+      const lastUsed = token.lastUsedAt.getTime();
+      const expirationDate = lastUsed + token.timeoutMs;
+      const now = Date.now();
+
+      if (now > expirationDate) {
+        throw new ApiError(
+          StatusCodes.UNAUTHORIZED,
+          ErrorEntries.INVALID_TOKEN,
+        );
+      }
+
+      if (!requiredScopes.every((scope) => token.scopes.includes(scope))) {
+        throw new ApiError(
+          StatusCodes.UNAUTHORIZED,
+          ErrorEntries.INSUFFICIENT_PERMISSIONS,
+        );
+      }
+
+      token.lastUsedAt = new Date(now);
+      await token.save();
+      (req as any).apiToken = token;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
