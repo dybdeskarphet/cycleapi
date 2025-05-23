e71bbe08e8d53a95db07c9feb58d23aceaa99e80
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-05-23 16:35:26 +0300

    refactor(express.types: Define SuccessEntry and ErrorEntry precisely

--- src/types/express.types.ts
+++ src/types/express.types.ts
@@ -1,5 +1,9 @@
-import { ErrorEntries } from "../constants/messages.constants";
-export type ErrorEntry = (typeof ErrorEntries)[keyof typeof ErrorEntries];
+export type ErrorEntry = {
+  message: string;
+  code: string;
+};
 
-import { SuccessEntries } from "../constants/messages.constants";
-export type SuccessEntry = (typeof SuccessEntries)[keyof typeof SuccessEntries];
+export type SuccessEntry = {
+  message: string;
+  code: string;
+};
